76b0ce4553ba4117eb40607f3e7e0b50929715bf
annerachael <annerwenji@gmail.com>
2024-05-01 10:09:28 -0700

    es6-basic

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * This function returns an object with two properties: an array of neighborhoods and a function that
+ * adds a new neighborhood to the array.
+ */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
